89b95cdef6e48590dbef1415dcd17b6ab9ce8726
dbw-studio <dbwangstudio@gmail.com>
2021-06-03 09:10:54 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+const Restaurant = require('./models/restaurant')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const restaurantList = require('./restaurant.json')
-
-const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const db = mongoose.connection
 
@@ -16,20 +15,45 @@ db.once('open', () => {
   console.log('mongodb conneted!!')
 })
 
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+// 新增的功能
+app.get('/restaurant/new', (req, res) => {
+  return res.render('new')
+})
 
-  res.render('show', { restaurant })
+app.post('/restaurant', (req, res) => {
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
 })
 
 app.get('/search', (req, res) => {
@@ -45,6 +69,8 @@ app.get('/search', (req, res) => {
     res.render('index', { restaurants, keyword })
   }
 })
+
+
 
 app.listen(3000, () => {
   console.log('Server is listening localhost:3000')
