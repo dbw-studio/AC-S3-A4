03d656a4df5e3a88cce76b49f8acb1e1a391d1d6
dbw-studio <dbwangstudio@gmail.com>
2021-06-03 12:47:16 +0800

    feat: revise search function

--- app.js
+++ app.js
@@ -95,6 +95,7 @@ app.post('/restaurants/:id/edit', (req, res) => {
 })
 
 // 刪除功能
+
 app.post('/restaurants/:id/delete', (req, res) => {
   const id = req.params.id
   return Restaurant.findById(id)
@@ -105,16 +106,27 @@ app.post('/restaurants/:id/delete', (req, res) => {
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().trim().includes(keyword.toLocaleLowerCase().trim()) || restaurant.category.toLowerCase().trim().includes(keyword.toLocaleLowerCase().trim())
-  })
-  const notice = `Sorry! 你搜尋的<span style="color:#4592af">${keyword}</span>，沒有相關餐廳。`
 
-  if (restaurants.length === 0) {
-    res.render('index', { keyword, notice })
-  } else {
-    res.render('index', { restaurants, keyword })
+  const notice = `Sorry! 你搜尋的<span style="color:#4592af">${keyword}</span>，沒有相關餐廳。<a href="/"><br><br><br><button class="btn btn-info">看全部餐廳</button></a>`
+  const emptyNotice = `Sorry! 無搜尋條件，請在搜尋欄輸入<span style="color:#4592af">餐廳名稱或分類</span>喔!<a href="/"><br><br><br><button class="btn btn-info">看全部餐廳</button></a>`
+
+  if(keyword.length === 0) {
+    res.render('index', { emptyNotice })
   }
+
+  return Restaurant.find()
+    .lean()
+    .then((restaurants) => {
+      const filteredRestaurant = restaurants.filter(restaurant => {
+        return restaurant.name.toLowerCase().trim().includes(keyword.toLocaleLowerCase().trim()) || restaurant.category.toLowerCase().trim().includes(keyword.toLocaleLowerCase().trim())})
+
+      if (filteredRestaurant.length === 0) {
+        res.render('index', { keyword, notice })
+      } else {
+        res.render('index', { restaurants: filteredRestaurant, keyword })
+      }
+    })
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
