86ad3ba0f99c465149f389ece2e93f88116857ec
dbw-studio <dbwangstudio@gmail.com>
2021-06-13 11:13:13 +0800

    feat: add filter (combine search and sort functions)

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -10,37 +10,28 @@ router.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
-// 排序
-router.get('/sort', (req, res) => {
-  const sortMethod = req.query.sortMethod
-  Restaurant.find()
-    .lean()
-    .sort(sortMethod)
-    .then(restaurants => res.render('index', { restaurants, sortMethod }))
-    .catch(error => console.log(error))
-})
-
-// 搜尋
-router.get('/search', (req, res) => {
+// 搜尋、排序
+router.get('/filter', (req, res) => {
   const keyword = req.query.keyword
+  const sortMethod = req.query.sortMethod
 
   const notice = `Sorry! 你搜尋的<span style="color:#4592af">${keyword}</span>，沒有相關餐廳。<a href="/"><br><br><br><button class="btn btn-info">看全部餐廳</button></a>`
   const emptyNotice = 'Sorry! 無搜尋條件，請在搜尋欄輸入<span style="color:#4592af">餐廳名稱或分類</span>喔!<a href="/"><br><br><br><button class="btn btn-info">看全部餐廳</button></a>'
 
-  if (keyword.length === 0) {
-    return res.render('index', { emptyNotice })
-  }
-
   return Restaurant.find()
     .lean()
+    .sort(sortMethod)
     .then((restaurants) => {
       const filteredRestaurant = restaurants.filter(restaurant => {
-        return restaurant.name.toLowerCase().trim().includes(keyword.toLocaleLowerCase().trim()) || restaurant.category.toLowerCase().trim().includes(keyword.toLocaleLowerCase().trim())})
+        return restaurant.name.toLowerCase().trim().includes(keyword.toLocaleLowerCase().trim()) || restaurant.category.toLowerCase().trim().includes(keyword.toLocaleLowerCase().trim())
+      })
 
-      if (filteredRestaurant.length === 0) {
-        res.render('index', { keyword, notice })
-      } else {
-        res.render('index', { restaurants: filteredRestaurant, keyword })
+      if (sortMethod !== '_id' && filteredRestaurant.length > 0) {
+        res.render('index', { restaurants: filteredRestaurant, sortMethod, keyword })
+      } else if (keyword.length > 0 && filteredRestaurant.length === 0) {
+        res.render('index', { notice, keyword, sortMethod })
+      } else if (keyword.length === 0) {
+        res.render('index', { emptyNotice })
       }
     })
     .catch(error => console.log(error))
